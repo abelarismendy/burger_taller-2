78a3d075b0a0052571f5a4fd56f0a2bb093d85cf
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-09-04 08:58:25 -0500

    Class ProductoMenu

--- src/hamburguesas/ProductoMenu.java
+++ src/hamburguesas/ProductoMenu.java
@@ -1,5 +1,24 @@
 package hamburguesas;
 
-public class ProductoMenu {
 
+public class ProductoMenu implements Producto {
+    private String nombre;
+    private int precioBase;
+
+    public ProductoMenu(String nombre, int precioBase) {
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precioBase;
+    }
+
+    public String generarTextoFactura() {
+        return nombre + " $" + precioBase;
+    }
 }
