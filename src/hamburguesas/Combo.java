dd3d3466d989edcf3b9e5e1483f261c5ea6c3b8a
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-09-04 09:20:35 -0500

    Class Combo

--- src/hamburguesas/Combo.java
+++ src/hamburguesas/Combo.java
@@ -1,5 +1,38 @@
 package hamburguesas;
 
-public class Combo {
+import java.util.List;
+import java.util.ArrayList;
 
+
+public class Combo implements Producto {
+    private double descuento;
+    private String nombreCombo;
+    private List<ProductoMenu> itemsCombo;
+
+    public Combo(String nombreCombo, double descuento) {
+        this.nombreCombo = nombreCombo;
+        this.descuento = descuento;
+        this.itemsCombo = new ArrayList<ProductoMenu>();
+    }
+
+    public void agregarItemACombo(ProductoMenu itemCombo) {
+        itemsCombo.add(itemCombo);
+    }
+
+    public String getNombre() {
+        return nombreCombo;
+    }
+
+    public int getPrecio() {
+        int precio = 0;
+        for (ProductoMenu item : itemsCombo) {
+            precio += item.getPrecio();
+        }
+        return (int) (precio * (1 - descuento));
+    }
+
+    public String generarTextoFactura() {
+        String texto = nombreCombo + " $" + getPrecio();
+        return texto;
+    }
 }
